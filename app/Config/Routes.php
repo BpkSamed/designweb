ee355f26b728c231518513f21f8d3075c07164ff
samed <MY_NAME@example.com>
2023-07-20 12:22:18 +0700

    perubahan routes 2

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -11,8 +11,8 @@ $routes = Services::routes();
  * --------------------------------------------------------------------
  */
 $routes->setDefaultNamespace('App\Controllers');
-$routes->setDefaultController('Home');
-$routes->setDefaultMethod('index');
+$routes->setDefaultController('control_project');
+$routes->setDefaultMethod('hal_pertama');
 $routes->setTranslateURIDashes(false);
 $routes->set404Override();
 // The Auto Routing (Legacy) is very dangerous. It is easy to create vulnerable apps
